9a45414d969821ecdddd0bd0f56f1e5ac020380c
Lormond <jangru@mail.ru>
2023-03-24 17:58:14 +0300

    Added the solution

---
+++ First_task/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] FillArray(int size)
+{
+  string[] testArray = new string[size];
+  Random nr = new Random();
+  for (int i = 0; i < size; i++)
+    testArray[i] = Convert.ToString(nr.Next(0, 10000));
+  return testArray;
+}
+
+string[] GetStringsWithRequiredLength(string[] inputArray, int maxLength)
+{
+  string[] resultArray = new string[inputArray.Length];
+  int j = 0;
+  for (int i = 0; i < inputArray.Length; i++)
+  {
+    if (inputArray[i].Length <= maxLength)
+    {
+      resultArray[j] = inputArray[i];
+      j++;
+    }
+  }
+  Array.Resize(ref resultArray, j);
+  return resultArray;
+}
+
+string[] sourceArray = FillArray(50);
+string[] resultString = GetStringsWithRequiredLength(sourceArray, 2);
+Console.WriteLine($"{string.Join(", ", resultString)}");
